716cade772abfbd4d320f660f59e1a0394158ffd
Boris Cherny <bcherny@gmail.com>
2016-11-15 21:39:23 -0800

    crawl (wip)

--- noCircularImportsRule.ts
+++ noCircularImportsRule.ts
@@ -1,6 +1,7 @@
-import { basename } from 'path'
 import * as ts from 'typescript'
 import * as Lint from 'tslint/lib/lint'
+import { readFile } from 'fs-promise'
+import { basename, resolve } from 'path'
 
 export class Rule extends Lint.Rules.AbstractRule {
   static FAILURE_STRING = 'Circular import detected'
@@ -21,54 +22,69 @@ export class Rule extends Lint.Rules.AbstractRule {
   }
 }
 
-const imports = new Map<string, Set<string>>()
-
 class NoCircularImportsWalker extends Lint.RuleWalker {
 
-  visitImportDeclaration(node: ts.ImportDeclaration) {
+  async visitImportDeclaration(node: ts.ImportDeclaration) {
 
-    const parent = node.parent as any
-    const thisModuleName = parent.fileName
-    const importPath = (node.moduleSpecifier as any).text
-    const importModule = parent.resolvedModules[importPath]
+    const parent = node.parent as ts.SourceFile
+    const importPath = (node.moduleSpecifier as any).text as string
+    const importModule = (parent as any).resolvedModules[importPath]
     const importCanonicalName = importModule.resolvedFileName as string
 
+    const thisModuleFileName = parent.fileName
+    const importedFileName = (node.moduleSpecifier as any).text as string
+    const fullImportedFileName = resolve(thisModuleFileName, importedFileName)
+    const importedFileNode = await this.getImports(fullImportedFileName)
+
+    console.log('imports', importedFileNode.importedFiles)
+
+
+    debugger
+
+
     // add to import graph
-    this.addToGraph(thisModuleName, importCanonicalName)
+    // this.addToGraph(thisModuleFileName, importCanonicalName)
 
     // check for cycles
-    if (this.hasCycle(thisModuleName)) {
-      this.addFailure(
-        this.createFailure(node.getStart(), node.getWidth(), `${Rule.FAILURE_STRING}: ${
-          this.getCycle(thisModuleName).concat(thisModuleName).map(_ => basename(_)).join(' -> ')
-        }`)
-      )
-    }
+    // if (this.hasCycle(thisModuleName)) {
+    //   this.addFailure(
+    //     this.createFailure(node.getStart(), node.getWidth(), `${Rule.FAILURE_STRING}: ${
+    //       this.getCycle(thisModuleName).concat(thisModuleName).map(_ => basename(_)).join(' -> ')
+    //     }`)
+    //   )
+    // }
 
     super.visitImportDeclaration(node)
+  }
+
+  private async getImports(fileName: string): Promise<string[]> {
+    const importedFileContents = await readFile(fileName, 'utf-8')
+    const importedFileNode = ts.preProcessFile(importedFileContents, true, true)
+    console.log('importedFileNode', importedFileNode)
+    return []
   }
 
   /**
    * TODO: don't rely on import name
    */
-  private addToGraph(thisModuleName: string, importCanonicalName: string) {
-    if (!imports.get(thisModuleName)) {
-      imports.set(thisModuleName, new Set)
-    }
-    imports.get(thisModuleName)!.add(importCanonicalName)
-  }
+  // private addToGraph(thisModuleName: string, importCanonicalName: string) {
+  //   if (!imports.get(thisModuleName)) {
+  //     imports.set(thisModuleName, new Set)
+  //   }
+  //   imports.get(thisModuleName)!.add(importCanonicalName)
+  // }
 
-  private hasCycle(moduleName: string): boolean {
-    return this.getCycle(moduleName).length > 0
-  }
+  // private hasCycle(moduleName: string): boolean {
+  //   return this.getCycle(moduleName).length > 0
+  // }
 
-  private getCycle(moduleName: string, accumulator: string[] = []): string[] {
-    if (!imports.get(moduleName)) return []
-    if (accumulator.includes(moduleName)) return accumulator
-    return Array.from(imports.get(moduleName) !.values()).reduce((_prev, _) => {
-      const c = this.getCycle(_, accumulator.concat(moduleName))
-      return c.length ? c : []
-    }, [] as string[])
-  }
+  // private getCycle(moduleName: string, accumulator: string[] = []): string[] {
+  //   if (!imports.get(moduleName)) return []
+  //   if (accumulator.includes(moduleName)) return accumulator
+  //   return Array.from(imports.get(moduleName) !.values()).reduce((_prev, _) => {
+  //     const c = this.getCycle(_, accumulator.concat(moduleName))
+  //     return c.length ? c : []
+  //   }, [] as string[])
+  // }
 
 }
