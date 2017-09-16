0b3999e5707add1c1e5b4fd026b58627e924c3d2
Boris Cherny <boris@performancejs.com>
2017-09-15 17:09:57 -0700

    fix test

--- test/test.ts
+++ test/test.ts
@@ -4,7 +4,9 @@ import * as assert from 'assert'
 console.log(__dirname)
 
 exec('../node_modules/.bin/tslint -c ./tslint.json -r ../ ./*.ts', { cwd: __dirname }, (error, stdout, stderr) => {
-  assert.equal(stdout, `case1.ts[1, 1]: Circular import detected: case1.ts -> case1.1.ts -> case1.ts
-case1.ts[2, 1]: Circular import detected: case1.ts -> case1.2.ts -> case1.ts
+  assert.equal(stdout, `
+ERROR: case1.ts[1, 1]: circular import detected: case1.ts -> case1.1.ts -> case1.ts
+ERROR: case1.ts[2, 1]: circular import detected: case1.ts -> case1.2.ts -> case1.ts
+
 `)
 })
