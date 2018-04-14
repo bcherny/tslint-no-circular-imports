69ff34255a744cbe07bcd880fe79fbaf20286d65
Boris Cherny <boris@performancejs.com>
2018-04-14 16:35:27 -0700

    Revert "Use platform specific directory separators to allow tests to be run o…"

--- test/test.ts
+++ test/test.ts
@@ -1,14 +1,9 @@
 import { exec } from 'child_process'
 import * as assert from 'assert'
-import { join } from 'path'
 
 console.log(__dirname)
 
-const tslintBin = join('..', 'node_modules', '.bin', 'tslint')
-const tslintConfig = join('.', 'tslint.json')
-const tsFiles = join('.', '*.ts');
-
-exec(`${tslintBin} -c ${tslintConfig} -r .. ${tsFiles}`, { cwd: __dirname }, (error, stdout, stderr) => {
+exec('../node_modules/.bin/tslint -c ./tslint.json -r ../ ./*.ts', { cwd: __dirname }, (error, stdout, stderr) => {
   assert.equal(stdout, `
 ERROR: case1.ts[1, 1]: circular import detected: case1.ts -> case1.1.ts -> case1.ts
 ERROR: case1.ts[2, 1]: circular import detected: case1.ts -> case1.2.ts -> case1.ts
