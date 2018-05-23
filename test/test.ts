5095e93573d933b7de4ad13efa94ae5bac7a736d
Gean Carlo Peixoto <geancpeixoto@gmail.com>
2018-05-23 19:45:48 -0300

    Rewrite tests using mocha

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,4 @@
+import 'mocha';
 import * as assert from 'assert'
 import { exec } from 'child_process'
 import { join } from 'path'
@@ -6,41 +7,64 @@ import * as Lint from 'tslint';
 const tslintBin = join('..', 'node_modules', '.bin', 'tslint')
 const tslintConfig = join('.', 'tslint.json')
 const tslintFormat = 'json'
-const tsFiles = `${join('.', '*.ts')} ${join('.', '*/*.ts')}`;
 const tsconfig = join('.', 'tsconfig.json');
 
-exec(`${tslintBin} -p ${tsconfig} -c ${tslintConfig} -r .. -t ${tslintFormat} ${tsFiles}`, { cwd: __dirname }, (error, stdout, stderr) => {
-  // Only validate failures and names.
-  const actual = (JSON.parse(stdout) as Lint.IRuleFailureJson[])
-      .map(x => ({ failure: x.failure, name: x.name }))
+/**
+ * exec tslint and return errors as json
+ */
+function run(filename: string, cb: (result: { failure: string, name: string }[]) => void) {
+  return (done: MochaDone) => {
+    exec(`${tslintBin} -p ${tsconfig} -c ${tslintConfig} -r .. -t ${tslintFormat} ${filename}`, { cwd: __dirname }, (_error, stdout, _stderr) => {
+      try {
+        const result = (JSON.parse(stdout) as Lint.IRuleFailureJson[])
+          .map(x => ({ failure: x.failure, name: x.name }));
+        cb(result);
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  }
+}
 
-  assert.deepEqual(actual, [
-    // case1
-    {
-      failure: 'circular import detected: case1.ts -> case1.2.ts -> case1.ts',
-      name: join(__dirname, 'case1.ts')
-    },
-    {
-      failure: 'circular import detected: case1.1.ts -> case1.ts -> case1.1.ts',
-      name: join(__dirname, 'case1.1.ts')
-    },
+describe('test.ts', () => {
+  it('case1', run('*.ts', value => {
+    assert.deepEqual(value, [
+      {
+        failure: 'circular import detected: case1.ts -> case1.2.ts -> case1.ts',
+        name: join(__dirname, 'case1.ts')
+      },
+      {
+        failure: 'circular import detected: case1.1.ts -> case1.ts -> case1.1.ts',
+        name: join(__dirname, 'case1.1.ts')
+      },
+    ]);
+  }));
 
-    // case2
-    {
-      failure: 'circular import detected: case2/a.ts -> case2/b.ts -> case2/a.ts',
-      name: join(__dirname, 'case2/a.ts')
-    },
+  it('case2', run('case2/*.ts', value => {
+    assert.deepEqual(value, [
+      {
+        failure: 'circular import detected: case2/a.ts -> case2/b.ts -> case2/a.ts',
+        name: join(__dirname, 'case2/a.ts')
+      },
+    ]);
+  }));
 
-    // case3
-    {
-      failure: 'circular import detected: case3/a.ts -> case3/b.ts -> case3/a.ts',
-      name: join(__dirname, 'case3/a.ts')
-    },
+  it('case3', run('case3/*.ts', value => {
+    assert.deepEqual(value, [
+      {
+        failure: 'circular import detected: case3/a.ts -> case3/b.ts -> case3/a.ts',
+        name: join(__dirname, 'case3/a.ts')
+      },
+    ]);
+  }));
 
-    // case4
-    {
-      failure: 'circular import detected: case4/a.ts -> case4/index.ts -> case4/a.ts',
-      name: join(__dirname, 'case4/a.ts')
-    }
-  ])
-})
+  it('case4', run('case4/*.ts', value => {
+    assert.deepEqual(value, [
+      {
+        failure: 'circular import detected: case4/a.ts -> case4/index.ts -> case4/a.ts',
+        name: join(__dirname, 'case4/a.ts')
+      }
+    ]);
+  }));
+});
